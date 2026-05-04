6005997562911195755ba5bb884d85c400943579
celia-mlx <marleix.celia@gmail.com>
2026-05-04 16:30:11 +0200

    Enhance testbench with clock generation and stimuli
    Added clock signal generation and adjusted stimulus timing for testing.

--- tb_network.vhd
+++ tb_network.vhd
@@ -7,28 +7,48 @@ ENTITY tb_network IS
 END ENTITY;
 
 ARCHITECTURE sim OF tb_network IS
+	SIGNAL clk_test    : std_logic := '0';
     SIGNAL input_test  : tab_int_const := (OTHERS => 0);
     SIGNAL output_test : tab_layer(1 TO 2);  -- remet tab_layer
+	CONSTANT period : time := 10 ns;
+
 BEGIN
     DUT : ENTITY work.network
         GENERIC MAP (Nb_Layers => 3)
         PORT MAP (
+			clk    => clk_test, 
             input  => input_test,
             output => output_test
         );
-    PROCESS
+
+	-- G√©n√©ration de l'horloge
+    clk_process : PROCESS
     BEGIN
-        input_test <= (0,0,0,0,0,0,0,0);
+        clk_test <= '0';
+        WAIT FOR period / 2;
+        clk_test <= '1';
+        WAIT FOR period / 2;
+    END PROCESS;
+			
+    -- Processus de stimuli
+    stimuli_process : PROCESS
+    BEGIN
+        -- Initialisation
+        input_test <= (OTHERS => 0);
         WAIT FOR 20 ns;
+        -- T1 : Entr√©es faibles
         input_test <= (1,0,1,0,1,0,1,0);
-        WAIT FOR 20 ns;
+        WAIT FOR 100 ns; -- On attend plus longtemps pour laisser le signal traverser les couches
+        -- T2 : Entr√©es moyennes
         input_test <= (1,1,1,1,1,1,1,1);
-        WAIT FOR 20 ns;
+        WAIT FOR 100 ns;
+        -- T3: Entr√©es fortes (Vmax probable)
         input_test <= (16,16,16,16,16,16,16,16);
-        WAIT FOR 20 ns;
-	-- Test 5 : entrÈes maximales rÈpÈtÈes
-	input_test <= (63,63,63,63,63,63,63,63);
-	WAIT FOR 20 ns;
+        WAIT FOR 100 ns;
+        -- T4 : Entr√©es maximales (6 bits)
+        input_test <= (63,63,63,63,63,63,63,63);
+        WAIT FOR 100 ns;
         WAIT;
     END PROCESS;
+
 END ARCHITECTURE;
